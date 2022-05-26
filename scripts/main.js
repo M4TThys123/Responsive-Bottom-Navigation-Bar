91a505042458acb126daec2e1480572770c39716
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-05-26 03:38:40 +0200

    final look on project

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,27 @@
-// navigation menu
-const primaryNav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".mobile-nav-toggle");
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
-navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute("data-visible");
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-    if (visibility === "false") {
-        primaryNav.setAttribute("data-visible", true)   
-    } else if (visibility === "true") {
-        primaryNav.setAttribute("data-visible", false)   
-    }
-});
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
 
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
